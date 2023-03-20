bc2a4187c1d5b6d6cce5f79a6be19ef98ac82a1a
Fillipe Madureira <39290405+fillipelgm@users.noreply.github.com>
2023-03-20 11:16:22 -0300

    added files

---
+++ linting.py
@@ -0,0 +1,18 @@
+import math
+import random
+
+
+def main(var_a, b, c, d):
+    my_list = ["apple", "banana", "cucumber", "mango"]
+    print("running main")
+
+    print(math.pi)
+    print(my_list[0:2])
+    print(f"a + b + c = {var_a+b+c}")
+
+
+if __name__ == "__main__":
+    main(2, 3, 4)
+    print()
+
+    print()
