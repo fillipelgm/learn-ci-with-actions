b7e6c4c9fcd0e1e0b975793e7e72375236151f7e
Fillipe Madureira <39290405+fillipelgm@users.noreply.github.com>
2023-03-20 15:20:40 -0300

    fixed linting.py

--- linting.py
+++ linting.py
@@ -1,18 +1,26 @@
-import random
 import math
+import random
 
 
-def main(var_a, b, c, d):
+def main(a: int, b: int, c: int, d: int):  # pylint: disable=unused-argument
+    """Sum and print the input integers.
+
+    Args:
+        a (int): arg 1
+        b (int): arg 2
+        c (int): arg 3
+        d (int): arg 4
+    """
     my_list = ["apple", "banana", "cucumber", "mango"]
     print("running main")
 
     print(math.pi)
     print(my_list[0:2])
-    print(f"a + b + c = {var_a+b+c}")
+    print(f"a + b + c = {a+b+c}")
 
 
 if __name__ == "__main__":
     main(2, 3, 4)
-    print()
+    print(random.uniform())
 
     print()
